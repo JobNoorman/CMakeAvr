82e71b80cc8e9a878dbaa69642f645578b491fd4
Job Noorman <job@noorman.info>
2016-11-05 17:59:32 +0100

    Add support for LTO builds
    This can/should probably be removed once LTO support for GCC is added to CMake.
    
    See the following links for reference:
    - https://gitlab.kitware.com/cmake/cmake/issues/15245
    - https://github.com/ngladitz/cmake/commit/a1ad4d555495b4055b78c29020050f06b668d96b

--- Platform/Generic-GNU-AVR.cmake
+++ Platform/Generic-GNU-AVR.cmake
@@ -7,8 +7,23 @@ set(__COMPILER_GNU_AVR 1)
 set(AVR_MCU   "" CACHE STRING "AVR MCU to target")
 set(AVR_F_CPU "" CACHE STRING "Clock frequency of the target AVR MCU")
 
+option(LTO "Enable Link-Time Optimization" ON)
+
 if (AVR_F_CPU)
     add_definitions(-DF_CPU=${AVR_F_CPU})
+endif ()
+
+# CMake does not support LTO/IPO yet for GCC. Until it does, we enable it
+# manually here.
+if (LTO)
+    # We need to use gcc-ar and gcc-ranlib for creating static libraries. Note
+    # that we cannot simply store the result of find_program in the CMAKE_*
+    # variables because these have already been set to ar and ranlib which means
+    # find_program would not search for them again.
+    find_program(AVR_GCC_AR NAMES avr-gcc-ar)
+    find_program(AVR_GCC_RANLIB NAMES avr-gcc-ranlib)
+    set(CMAKE_AR ${AVR_GCC_AR})
+    set(CMAKE_RANLIB ${AVR_GCC_RANLIB})
 endif ()
 
 macro(__compiler_gnu_avr lang)
@@ -21,5 +36,9 @@ macro(__compiler_gnu_avr lang)
 
     # Default extension is .obj if not on UNIX; make it .o
     set(CMAKE_${lang}_OUTPUT_EXTENSION .o)
+
+    if (LTO)
+        set(CMAKE_${lang}_FLAGS_INIT "${CMAKE_${lang}_FLAGS_INIT} -flto")
+    endif ()
 endmacro()
 
