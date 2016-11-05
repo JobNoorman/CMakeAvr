079279502b747b8d1a7e29fe0333bc26efcd7d3e
Job Noorman <job@noorman.info>
2016-11-05 20:27:12 +0100

    Add function to generate an upload target
    This is currently pretty simplistic. For example, all parameters like the port
    or baudrate are global and cannot be configured per target. This should
    probably be fixed in the near future.
    
    The function will give a warning and not create the target when avrdude is not
    found or the port has not been set by the user. The reason that we only err on
    a missing port and not on missing baudrate or programmer type is that assuming
    a default port seems to be the most dangerous because we might accidentally
    start talking to a random device. If the user has already consciously set the
    port, we assume they have thought about whether or not to use the default
    values for the other parameters.

---
+++ AvrUpload.cmake
@@ -0,0 +1,32 @@
+function(add_upload_target target)
+    find_program(AVRDUDE_EXE NAMES avrdude)
+
+    if (NOT AVRDUDE_EXE)
+        message(WARNING "Cannot find avrdude; not adding upload target "
+                        "for ${target}")
+        return()
+    endif ()
+
+    if (NOT AVR_UPLOAD_PORT)
+        message(WARNING "AVR_UPLOAD_PORT not set; not adding upload target "
+                        "for ${target}")
+        return()
+    endif ()
+
+    set(AVR_UPLOAD_BAUD 115200 CACHE STRING "Baudrate for the upload port")
+    set(AVR_UPLOAD_PROGRAMMER arduino CACHE STRING "Programmer for uploading")
+
+    add_custom_target(${target}-upload
+        COMMAND ${AVRDUDE_EXE}
+                -p ${AVR_MCU}
+                -c ${AVR_UPLOAD_PROGRAMMER}
+                -P ${AVR_UPLOAD_PORT}
+                -b ${AVR_UPLOAD_BAUD}
+                -U flash:w:$<TARGET_FILE:${target}>:e
+        COMMENT "Uploading target ${target}"
+        VERBATIM
+        USES_TERMINAL)
+
+    add_dependencies(${target}-upload ${target})
+endfunction()
+
