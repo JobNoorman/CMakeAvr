e5561ffcd9b0981d26184d2ab8db08766f1fd31c
Job Noorman <job@noorman.info>
2016-11-05 15:20:12 +0100

    Move some common code to a macro
    This uses the same technique as the existing compiler modules (see, for
    example, Compiler/GNU.cmake).

--- Platform/Generic-GNU-AVR.cmake
+++ Platform/Generic-GNU-AVR.cmake
@@ -1,14 +1,25 @@
+# This module is shared by multiple languages; use include blocker.
+if(__COMPILER_GNU_AVR)
+    return()
+endif()
+set(__COMPILER_GNU_AVR 1)
+
 set(AVR_MCU   "" CACHE STRING "AVR MCU to target")
 set(AVR_F_CPU "" CACHE STRING "Clock frequency of the target AVR MCU")
-
-if (AVR_MCU)
-    set(AVR_GNU_GENERIC_FLAGS -mmcu=${AVR_MCU})
-endif ()
 
 if (AVR_F_CPU)
     add_definitions(-DF_CPU=${AVR_F_CPU})
 endif ()
 
-# Flags for the MinSizeDebug build type
-set(AVR_GNU_GENERIC_FLAGS_MINSIZEDEBUG "-g -Os")
+macro(__compiler_gnu_avr lang)
+    if (AVR_MCU)
+        set(CMAKE_${lang}_FLAGS_INIT -mmcu=${AVR_MCU})
+    endif ()
+
+    # Flags for the MinSizeDebug build type
+    set(CMAKE_${lang}_FLAGS_MINSIZEDEBUG "-g -Os")
+
+    # Default extension is .obj if not on UNIX; make it .o
+    set(CMAKE_${lang}_OUTPUT_EXTENSION .o)
+endmacro()
 
