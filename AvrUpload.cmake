6ab00596689da96dae43d919b0c2ed8e477bf2d7
Job Noorman <job@noorman.info>
2017-08-01 21:03:23 +0200

    Add per-target upload settings
    The global port/baud rate/programmer settings still exist but can now be
    overridden per target.

--- AvrUpload.cmake
+++ AvrUpload.cmake
@@ -1,6 +1,6 @@
-set(AVR_UPLOAD_PORT       ""      CACHE STRING "Port for uploading")
-set(AVR_UPLOAD_BAUD       115200  CACHE STRING "Baudrate for the upload port")
-set(AVR_UPLOAD_PROGRAMMER arduino CACHE STRING "Programmer for uploading")
+set(AVR_UPLOAD_PORT       ""      CACHE STRING "Defauly port for uploading")
+set(AVR_UPLOAD_BAUD       115200  CACHE STRING "Default baudrate for the upload port")
+set(AVR_UPLOAD_PROGRAMMER arduino CACHE STRING "Default programmer for uploading")
 
 function(add_upload_target target)
     find_program(AVRDUDE_EXE NAMES avrdude)
@@ -11,18 +11,29 @@ function(add_upload_target target)
         return()
     endif ()
 
-    if (NOT AVR_UPLOAD_PORT)
-        message(WARNING "AVR_UPLOAD_PORT not set; not adding upload target "
-                        "for ${target}")
+    set(port_var       AVR_${target}_UPLOAD_PORT)
+    set(baud_var       AVR_${target}_UPLOAD_BAUD)
+    set(programmer_var AVR_${target}_UPLOAD_PROGRAMMER)
+
+    set(${port_var} ${AVR_UPLOAD_PORT}
+        CACHE STRING "Port for uploading target ${target}")
+    set(${baud_var} ${AVR_UPLOAD_BAUD}
+        CACHE STRING "Baudrate for the upload port for target ${target}")
+    set(${programmer_var} ${AVR_UPLOAD_PROGRAMMER}
+        CACHE STRING "Programmer for uploading target ${target}")
+
+    if (NOT ${port_var})
+        message(WARNING "Upload port not set for target ${target}; "
+                        "not adding upload target ")
         return()
     endif ()
 
     add_custom_target(${target}-upload
         COMMAND ${AVRDUDE_EXE}
                 -p ${AVR_MCU}
-                -c ${AVR_UPLOAD_PROGRAMMER}
-                -P ${AVR_UPLOAD_PORT}
-                -b ${AVR_UPLOAD_BAUD}
+                -c ${${programmer_var}}
+                -P ${${port_var}}
+                -b ${${baud_var}}
                 -U flash:w:$<TARGET_FILE:${target}>:e
         COMMENT "Uploading target ${target}"
         VERBATIM
